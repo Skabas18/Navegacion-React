f3f5621fd9bd230da53df6187c8d4a99c9898f9a
Sebastian Cabal <sebascabalq@gmail.com>
2024-04-03 07:46:00 -0500

    Creación de rutas dinamicas+

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import { blogdata } from './blogdata';
 function BlogPage() {
     return (
-        <div>BlogPage</div>
+        <>
+            <h1>BlogPage</h1>
+            {blogdata.map(post => (
+                <BlogLink post={post} />
+            ))}
+        </>
     )
 }
+
+function BlogLink({ post }) {
+    return (
+        <li>
+            <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+        </li>
+    );
+}
+
 
 export { BlogPage }
